3482c9da57613e0cb4cafcb47c9417cea92ed298
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-02-12 10:59:02 -0500

    Feature: VFQ IDS Codebook (#20)

---
+++ vista-fhir-query-ids-mapping/src/main/java/gov/va/api/health/vistafhirquery/idsmapping/VistaFhirQueryIdsCodebookSupplier.java
@@ -0,0 +1,24 @@
+package gov.va.api.health.vistafhirquery.idsmapping;
+
+import gov.va.api.health.ids.client.EncryptingIdEncoder.Codebook;
+import gov.va.api.health.ids.client.EncryptingIdEncoder.Codebook.Mapping;
+import gov.va.api.health.ids.client.EncryptingIdEncoder.CodebookSupplier;
+import java.util.List;
+
+/** Shared mapping to be used by Vista-Fhir-Query. */
+public class VistaFhirQueryIdsCodebookSupplier implements CodebookSupplier {
+  @Override
+  public Codebook get() {
+    return Codebook.builder()
+        .map(
+            List.of(
+                /* Systems */
+                Mapping.of("VISTA", "V"),
+                Mapping.of("MPI", "M"),
+                Mapping.of("UNKNOWN", "U"),
+                /* Resources*/
+                Mapping.of("OBSERVATION", "OB"),
+                Mapping.of("PATIENT", "PA")))
+        .build();
+  }
+}
