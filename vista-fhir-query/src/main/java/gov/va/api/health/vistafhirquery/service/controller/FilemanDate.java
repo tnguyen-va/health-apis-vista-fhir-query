c2da7857f1a6225eec5f141b321b2250bfeef42b
JJ Abando <jesse.abando@libertyits.com>
2021-02-18 11:59:10 -0500

    static constructors, integer and date checks

--- vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/FilemanDate.java
+++ vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/FilemanDate.java
@@ -2,54 +2,89 @@ package gov.va.api.health.vistafhirquery.service.controller;
 
 import gov.va.api.lighthouse.vistalink.models.ValueOnlyXmlAttribute;
 import java.time.Instant;
-import lombok.extern.slf4j.Slf4j;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.regex.PatternSyntaxException;
 
-@Slf4j
 public class FilemanDate {
   private final Instant instant;
 
-  FilemanDate(Instant i) {
+  public FilemanDate(Instant i) {
     this.instant = i;
   }
 
-  FilemanDate(ValueOnlyXmlAttribute valueOnlyXmlAttribute) {
-    this.instant = parse(valueOnlyXmlAttribute.value());
+  public static FilemanDate from(Instant i) {
+    return new FilemanDate(i);
   }
 
-  FilemanDate(String filemanDate) {
-    this.instant = parse(filemanDate);
+  public static FilemanDate from(ValueOnlyXmlAttribute valueOnlyXmlAttribute) {
+    return new FilemanDate(parse(valueOnlyXmlAttribute.value()));
+  }
+
+  public static FilemanDate from(String filemanDate) {
+    return new FilemanDate(parse(filemanDate));
+  }
+
+  private static Instant parse(String filemanDate) {
+    String numberpattern = "[0-9]+";
+    int hour = 0;
+    int minute = 0;
+    int second = 0;
+    if (filemanDate.contains(".")) {
+      String[] splitDate = filemanDate.split("\\.", -1);
+      String tail = splitDate[1];
+      if (tail.length() >= 2) {
+        String maybeHour = tail.substring(0, 2);
+        if (!maybeHour.matches(numberpattern)) {
+          throw new PatternSyntaxException("Hour value cannot be parsed.", numberpattern, -1);
+        }
+        hour = Integer.parseInt(maybeHour);
+      }
+      if (tail.length() >= 4) {
+        String maybeMinute = tail.substring(2, 4);
+        if (!maybeMinute.matches(numberpattern)) {
+          throw new PatternSyntaxException("Minute value cannot be parsed.", numberpattern, -1);
+        }
+        minute = Integer.parseInt(maybeMinute);
+      }
+      if (tail.length() == 6) {
+        String maybeSecond = tail.substring(4, 6);
+        if (!maybeSecond.matches(numberpattern)) {
+          throw new PatternSyntaxException("Second value cannot be parsed.", numberpattern, -1);
+        }
+        second = Integer.parseInt(maybeSecond);
+      }
+    }
+    int year = 0;
+    int month = 0;
+    int day = 0;
+    if (filemanDate.length() >= 3) {
+      String maybeYear = filemanDate.substring(0, 3);
+      if (!maybeYear.matches(numberpattern)) {
+        throw new PatternSyntaxException("Year value cannot be parsed.", numberpattern, -1);
+      }
+      year = Integer.parseInt(maybeYear) + 1700;
+    }
+    if (filemanDate.length() >= 5) {
+      String maybeMonth = filemanDate.substring(3, 5);
+      if (!maybeMonth.matches(numberpattern)) {
+        throw new PatternSyntaxException("Month value cannot be parsed.", numberpattern, -1);
+      }
+      month = Integer.parseInt(maybeMonth);
+    }
+    if (filemanDate.length() >= 7) {
+      String maybeDay = filemanDate.substring(5, 7);
+      if (!maybeDay.matches(numberpattern)) {
+        throw new PatternSyntaxException("Day value cannot be parsed.", numberpattern, -1);
+      }
+      day = Integer.parseInt(maybeDay);
+    }
+    return ZonedDateTime.of(year, month, day, hour, minute, second, 0, ZoneId.of("UTC"))
+        .toInstant();
   }
 
   Instant asInstant() {
     return instant;
-  }
-
-  @SuppressWarnings("StringSplitter")
-  Instant parse(String filemanDate) {
-    String hour = "00";
-    String minute = "00";
-    String second = "00";
-    if (filemanDate.contains(".")) {
-      String[] splitDate = filemanDate.split("\\.");
-      String tail = splitDate[1];
-      log.info("TAIL: " + tail);
-      if (tail.length() >= 2) {
-        hour = tail.substring(0, 2);
-        log.info("HOUR: " + hour);
-      }
-      if (tail.length() >= 4) {
-        minute = tail.substring(2, 4);
-      }
-      if (tail.length() == 6) {
-        second = tail.substring(4, 6);
-      }
-    }
-    String year = filemanDate.substring(0, 3);
-    year = String.valueOf(Integer.parseInt(year) + 1700);
-    String month = filemanDate.substring(3, 5);
-    String day = filemanDate.substring(5, 7);
-    return Instant.parse(
-        year + "-" + month + "-" + day + "T" + hour + ":" + minute + ":" + second + "Z");
   }
 
   public String toString() {
--- vista-fhir-query/src/test/java/gov/va/api/health/vistafhirquery/service/controller/FilemanDateTest.java
+++ vista-fhir-query/src/test/java/gov/va/api/health/vistafhirquery/service/controller/FilemanDateTest.java
@@ -1,34 +1,66 @@
 package gov.va.api.health.vistafhirquery.service.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import gov.va.api.lighthouse.vistalink.models.ValueOnlyXmlAttribute;
+import java.time.DateTimeException;
 import java.time.Instant;
+import java.util.regex.PatternSyntaxException;
 import org.junit.jupiter.api.Test;
 
 public class FilemanDateTest {
   @Test
+  void checkInvalidDates() {
+    assertThatExceptionOfType(DateTimeException.class)
+        .isThrownBy(() -> FilemanDate.from("2975019.082801"));
+    assertThatExceptionOfType(DateTimeException.class)
+        .isThrownBy(() -> FilemanDate.from("2970940.082801"));
+    assertThatExceptionOfType(DateTimeException.class)
+        .isThrownBy(() -> FilemanDate.from("2970919.302801"));
+    assertThatExceptionOfType(DateTimeException.class)
+        .isThrownBy(() -> FilemanDate.from("2970919.087001"));
+    assertThatExceptionOfType(DateTimeException.class)
+        .isThrownBy(() -> FilemanDate.from("2970919.082890"));
+  }
+
+  @Test
+  void checkInvalidStringCannotBeParsed() {
+    assertThatExceptionOfType(PatternSyntaxException.class)
+        .isThrownBy(() -> FilemanDate.from("aa70919.082801"));
+    assertThatExceptionOfType(PatternSyntaxException.class)
+        .isThrownBy(() -> FilemanDate.from("297bb19.082801"));
+    assertThatExceptionOfType(PatternSyntaxException.class)
+        .isThrownBy(() -> FilemanDate.from("29709cc.082801"));
+    assertThatExceptionOfType(PatternSyntaxException.class)
+        .isThrownBy(() -> FilemanDate.from("2970919.aa2801"));
+    assertThatExceptionOfType(PatternSyntaxException.class)
+        .isThrownBy(() -> FilemanDate.from("2970919.08cc01"));
+    assertThatExceptionOfType(PatternSyntaxException.class)
+        .isThrownBy(() -> FilemanDate.from("2970919.0828bb"));
+  }
+
+  @Test
   void createFilemanDatefromInstant() {
-    assertThat(new FilemanDate(Instant.parse("1997-09-19T08:27:01Z")).asInstant())
+    assertThat(FilemanDate.from(Instant.parse("1997-09-19T08:27:01Z")).asInstant())
         .isEqualTo(Instant.parse("1997-09-19T08:27:01Z"));
   }
 
   @Test
   void createFilemanDatefromString() {
-    assertThat(new FilemanDate("2970919").asInstant())
+    assertThat(FilemanDate.from("2970919").asInstant())
         .isEqualTo(Instant.parse("1997-09-19T00:00:00Z"));
-    assertThat(new FilemanDate("2970919.08").asInstant())
+    assertThat(FilemanDate.from("2970919.08").asInstant())
         .isEqualTo(Instant.parse("1997-09-19T08:00:00Z"));
-    assertThat(new FilemanDate("2970919.0827").asInstant())
+    assertThat(FilemanDate.from("2970919.0827").asInstant())
         .isEqualTo(Instant.parse("1997-09-19T08:27:00Z"));
-    assertThat(new FilemanDate("2970919.082701").asInstant())
+    assertThat(FilemanDate.from("2970919.082701").asInstant())
         .isEqualTo(Instant.parse("1997-09-19T08:27:01Z"));
   }
 
   @Test
   void createFilemanDatefromValueOnlyXmlAttribute() {
     assertThat(
-            new FilemanDate(ValueOnlyXmlAttribute.builder().value("2970919.082701").build())
+            FilemanDate.from(ValueOnlyXmlAttribute.builder().value("2970919.082701").build())
                 .asInstant())
         .isEqualTo(Instant.parse("1997-09-19T08:27:01Z"));
   }
