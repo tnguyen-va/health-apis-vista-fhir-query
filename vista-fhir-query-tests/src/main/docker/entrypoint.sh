8bd593993a4f17c568a6a6c27402225617e5cf92
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-03-03 09:03:17 -0500

    Docker-Entrypoint: Add Sentinel R4 properties (#44)

--- vista-fhir-query-tests/src/main/docker/entrypoint.sh
+++ vista-fhir-query-tests/src/main/docker/entrypoint.sh
@@ -14,10 +14,14 @@ main() {
   if [ -z "${SENTINEL_ENV:-}" ]; then SENTINEL_ENV="${K8S_ENVIRONMENT}"; fi
   if [ -z "${VFQ_URL:-}" ]; then VFQ_URL="https://${K8S_LOAD_BALANCER}"; fi
 
-  SYSTEM_PROPERTIES="-Dsentinel=${SENTINEL_ENV} -Dsentinel.internal.url=${VFQ_URL}"
+  SYSTEM_PROPERTIES="-Dsentinel=${SENTINEL_ENV} \
+    -Dsentinel.internal.url=${VFQ_URL} \
+    -Dsentinel.r4.url=${VFQ_URL}"
 
   if [ -n "${VFQ_API_PATH:-}" ]; then  addToSystemProperties "sentinel.internal.api-path" "${VFQ_API_PATH}"; fi
+  if [ -n "${VFQ_R4_API_PATH:-}" ]; then  addToSystemProperties "sentinel.r4.api-path" "${VFQ_R4_API_PATH}"; fi
   if [ -n "${VFQ_PORT:-}" ]; then addToSystemProperties "sentinel.internal.port" "${VFQ_PORT}"; fi
+  if [ -n "${VFQ_R4_PORT:-}" ]; then addToSystemProperties "sentinel.r4.port" "${VFQ_R4_PORT}"; fi
   if [ -n "${MAGIC_ACCESS_TOKEN:-}" ]; then addToSystemProperties "access-token" "${MAGIC_ACCESS_TOKEN}"; fi
   if [ -n "${VFQ_CLIENT_KEY:-}" ]; then addToSystemProperties "client-key" "${VFQ_CLIENT_KEY}"; fi
 
