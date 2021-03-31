325f0bf6a716b1b0e857e49f7d2431bc1d28d5eb
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-03-31 16:59:25 -0400

    Include API Path in IT(#71)

--- vista-fhir-query-tests/src/test/java/gov/va/api/health/vistafhirquery/tests/r4/ConformanceIT.java
+++ vista-fhir-query-tests/src/test/java/gov/va/api/health/vistafhirquery/tests/r4/ConformanceIT.java
@@ -1,22 +1,31 @@
 package gov.va.api.health.vistafhirquery.tests.r4;
 
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 import gov.va.api.health.r4.api.information.WellKnown;
 import gov.va.api.health.r4.api.resources.CapabilityStatement;
+import gov.va.api.health.vistafhirquery.tests.SystemDefinitions;
 import gov.va.api.health.vistafhirquery.tests.TestClients;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+@Slf4j
 public class ConformanceIT {
 
-  @Test
-  void metadata() {
-    TestClients.internal()
-        .get("/r4/.well-known/smart-configuration")
-        .expect(200)
-        .expectValid(WellKnown.class);
+  static Stream<Arguments> conformance() {
+    String apiPath = SystemDefinitions.systemDefinition().internal().apiPath();
+    return Stream.of(
+        arguments(apiPath + "r4/.well-known/smart-configuration", WellKnown.class),
+        arguments(apiPath + "r4/metadata", CapabilityStatement.class));
   }
 
-  @Test
-  void smartConfiguration() {
-    TestClients.internal().get("/r4/metadata").expect(200).expectValid(CapabilityStatement.class);
+  @ParameterizedTest
+  @MethodSource
+  void conformance(String path, Class<?> expectedClassType) {
+    log.info("Verify {} is {} ({})", path, expectedClassType.getSimpleName(), 200);
+    TestClients.internal().get(path).expect(200).expectValid(expectedClassType);
   }
 }
